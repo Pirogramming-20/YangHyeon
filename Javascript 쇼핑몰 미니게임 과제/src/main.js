048a52d765f3e02fd8d38bf8d164bb9d91dacc39
hyeonee <yhe01152@naver.com>
2024-01-03 19:03:36 +0900

    [javascript 1강- handling events]

--- Javascript 쇼핑몰 미니게임 과제/src/main.js
+++ Javascript 쇼핑몰 미니게임 과제/src/main.js
@@ -12,7 +12,6 @@ function loadItems() {
 function displayItems(items) {
   //item 내부의 <ul> 태그들의 html코드들을 받아온다.
   const container = document.querySelector(".items");
-  console.log(container);
   //ul 을 받아와서 li 태그로 달라진 것을 HTML 내부에 생성한다.
   //join을 이용하여 items 내부에서 생성된 여러 li 태그 안의 있는 값들을 합친다.
   // 아래의 items 들은 json 내부에 있는 items 로, 각각의 배열 안 모두를  li로 변환 시켜,
@@ -21,7 +20,6 @@ function displayItems(items) {
   //이때의 innerHTML 이란 받아온 <ul> 태그 안의 내부를 의미하는 것이다.
   //따라서 join 의 경우 ul 뒤가 아닌 내부에 붙게 된다.
   container.innerHTML = items.map((item) => createHTMLString(item)).join("");
-  console.log(container);
 }
 
 function createHTMLString(item) {
@@ -34,10 +32,37 @@ function createHTMLString(item) {
     </li>`;
 }
 
+//버튼 클릭시 발생하는 이벤트
+function onButtonClick(event, items) {
+  const dataset = event.target.dataset;
+  const key = dataset.key;
+  const value = dataset.value;
+
+  if (key == null || value == null) {
+    return;
+  }
+
+  //filter 을 이용하면 기존 박스를 계속 다시 재생성하여 붙여넣으므로 무거워진다.
+  const filtered = items.filter((item) => item[key] === value);
+  console.log(filtered);
+  displayItems(filtered);
+}
+
+function setEventListteners(items) {
+  const logo = document.querySelector(".logo");
+  //  const buttons = document.querySelector(".btn);
+  // 으로 작성하게 된다면, 첫 번째의 tshirts 만 작동하게 된다. 이는 querySelector 가 처음 것만 가져오기 때문이다.
+  const buttons = document.querySelector(".buttons");
+
+  //click은 string 의 역할이 아닌 옵션 지정자이다.
+  logo.addEventListener("click", () => displayItems(items));
+  buttons.addEventListener("click", (event) => onButtonClick(event, items));
+}
+
 //main
 loadItems()
   .then((items) => {
     displayItems(items);
-    //setEventListteners(items);
+    setEventListteners(items);
   })
   .catch(console.log());
