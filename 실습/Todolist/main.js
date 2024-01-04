d474baab67720c30650b8e6323921c8a725eea21
hyeonee <yhe01152@naver.com>
2024-01-04 15:42:53 +0900

    실습-piromate 오후 실습 종료

--- 실습/Todolist/main.js
+++ 실습/Todolist/main.js
@@ -30,6 +30,16 @@ function addTask() {
   listContainer.appendChild(li);
 }
 
+//데어터 저장하는 함수
+function saveData() {
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+
+//데이터를 불러오는 함수
+function showData() {
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+
 function confirmTask() {
   const taskInput = document.getElementById("taskInput");
   const task = taskInput.value;
@@ -43,5 +53,95 @@ function confirmTask() {
 
     listContainer.appendChild(li);
     listContainer.removeChild(taskInput.parentElement);
+    saveData();
   }
 }
+
+//체크표시 구현
+listContainer.addEventListener("click", (e) => {
+  e.target.classList.toggle("checked");
+
+  let checkedTodos = document.getElementById("today").innerText;
+
+  if (e.target.tagName === "LI") {
+    //변형해야해서 let 사용함.
+    if (e.target.classList.contains("checked")) {
+      checkedTodos = parseInt(checkedTodos) + 1;
+    } else if (checkedTodos != 0) {
+      checkedTodos = parseInt(checkedTodos) - 1;
+    }
+
+    saveData();
+    document.getElementById("today").innerText = checkedTodos;
+  }
+});
+
+showData();
+localStorage.clear();
+
+// const TODO_LIST_DATA = [
+//   "동아리",
+//   "후회없는 여름방학 보내기",
+//   "좋은 인연 많이 만들기",
+//   "헤맨만큼 내 땅이다! 마음껏 헤매기",
+//   "자식같은 프로젝트 해보기",
+// ];
+
+// TODO_LIST_DATA.forEach((item) => {
+//   const li = document.createElement("li");
+//   li.innerText = item;
+//   listContainer.appendChild(li);
+// });
+
+const TODO_LIST_DATA = [
+  {
+    category: "방학목표",
+    todolist: [
+      "후회없는 여름방학 보내기",
+      "좋은 인연 많이 만들기",
+      "헤맨만큼 내 땅이다! 마음껏 헤매기",
+      "자식같은 프로젝트 해보기",
+    ],
+  },
+  {
+    category: "학교",
+    todolist: ["등교하기", "하교하기"],
+  },
+];
+
+const main = document.getElementsByClassName("main")[0];
+
+TODO_LIST_DATA.forEach((block) => {
+  //category 태그
+  const category = document.createElement("div");
+  category.classList.add("category");
+
+  //카테고리 이름 태그
+  const categoryName = document.createElement("p");
+  categoryName.innerText = block.category;
+
+  //버튼 태그 만들기
+  const btn = document.createElement("div");
+  btn.classList.add("add-task-btn");
+  btn.setAttribute("id", "addTaskBtn");
+
+  const icon = document.createElement("i");
+  icon.classList.add("ri-add-line");
+
+  btn.appendChild(icon);
+  category.appendChild(categoryName);
+  category.appendChild(btn);
+  main.appendChild(category);
+
+  //리스트 컨테이너
+  const ul = document.createElement("ul");
+  ul.setAttribute("id", "listContainer");
+
+  block.todolist.forEach((item) => {
+    const li = document.createElement("li");
+    li.innerText = item;
+    ul.appendChild(li);
+  });
+
+  main.appendChild(ul);
+});
