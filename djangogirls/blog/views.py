e9eb50d7413ff98bb42cf32b6b47091c27b81189
hyeonee <yhe01152@naver.com>
2024-01-10 00:14:51 +0900

    Django 템플릿

--- djangogirls/blog/views.py
+++ djangogirls/blog/views.py
@@ -6,6 +6,7 @@ from .models import Post
 def post_list(request):
     # view 에 모델을 가져오는 작업. 오름 차순으로 Post 에서 가져온다.
     posts  = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    print(posts)
     # 윗 줄 즉 모델에서 가져온 posts 파일을 가져와 post_list.html 에서 render 을 이용해 화면에 띄워준다.
     return render(request, 'blog/post_list.html',{'posts' : posts})
 
