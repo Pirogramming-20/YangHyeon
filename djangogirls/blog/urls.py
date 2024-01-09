1f756d38ff50c591c9467007b30b5948458f1343
hyeonee <yhe01152@naver.com>
2024-01-09 22:30:53 +0900

    Django 뷰 만들기

--- djangogirls/blog/urls.py
+++ djangogirls/blog/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.post_list, name="post_list"),
+    path('',views.post_list, name="post_list"), # post_list 라는 view 가 루트 URL 에 추가되었다.
 ]
