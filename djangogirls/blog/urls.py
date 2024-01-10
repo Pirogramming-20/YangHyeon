20754cb29ecf736caee037a8a56d4f2fceaca7df
hyeonee <yhe01152@naver.com>
2024-01-10 17:18:58 +0900

    어플리케이션 확장하기

--- djangogirls/blog/urls.py
+++ djangogirls/blog/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.post_list, name="post_list"), # post_list 라는 view 가 루트 URL 에 추가되었다.
+    path('',views.post_list, name="post_list"), # post_list 라는 view 함수가 루트 URL 에 추가되었다.
+    # /post/3/ 가 예시로 입력된다면,,, views.post_detail 함수 실행
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
--- djangogirls/blog/views.py
+++ djangogirls/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 def post_list(request):
@@ -10,3 +11,9 @@ def post_list(request):
     # 윗 줄 즉 모델에서 가져온 posts 파일을 가져와 post_list.html 에서 render 을 이용해 화면에 띄워준다.
     return render(request, 'blog/post_list.html',{'posts' : posts})
 
+
+
+def post_detail(request, pk):
+    Post.objects.get(pk=pk)
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
