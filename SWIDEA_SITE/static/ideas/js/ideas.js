b2518ad240e9c6da9680545828ed1ce8fbc8b3cb
hyeonee <yhe01152@naver.com>
2024-01-18 07:07:28 +0900

    feat : 페이지네이션 구현

--- SWIDEA_SITE/static/ideas/js/ideas.js
+++ SWIDEA_SITE/static/ideas/js/ideas.js
@@ -2,6 +2,7 @@ function submitSelect() {
   document.getElementById("sort_form").submit();
 }
 
+/*---------------------------------------------------- */
 const csrftoken = document.querySelector("[name=csrfmiddlewaretoken]").value;
 function upinterst(pk, dir) {
   fetch("/ideas/", {
@@ -42,3 +43,66 @@ function downinterst(pk, dir) {
       console.error("Fetch error:", error);
     });
 }
+
+/*---------------------------------------------------- */
+const data = document.querySelectorAll(".idea-block");
+const totalnum = data.length;
+
+const blockcount = 4;
+const totalPageCount = Math.ceil(totalnum / blockcount);
+let currentpage = 1;
+
+const numlist_box = document.querySelector(".page_number_list");
+for (let i = 2; i <= totalPageCount; i++) {
+  numlist_box.innerHTML += `<span class="page_btn"> ${i} </span> `;
+}
+
+/*---------------------------------------------------- */
+
+const all_page_btn = document.querySelectorAll(".page_btn");
+
+function highlightPage() {
+  all_page_btn.forEach((btn) => {
+    if (btn.classList.contains("current-page")) {
+      btn.classList.remove("current-page");
+    }
+  });
+  all_page_btn[currentpage - 1].classList.add("current-page");
+}
+
+/*---------------------------------------------------- */
+const box_list = document.querySelector(".idea-block-list");
+const allBlock = document.querySelectorAll(".idea-block");
+
+function createPage(num) {
+  highlightPage();
+  box_list.innerHTML = "";
+
+  for (let i = blockcount * (num - 1); i < blockcount * num; i++) {
+    if (allBlock[i].innerHTML != undefined) {
+      const block = document.createElement("div");
+      block.classList.add("idea-block");
+      block.innerHTML = allBlock[i].innerHTML;
+      box_list.appendChild(block);
+    } else break;
+  }
+}
+
+createPage(1);
+
+const before_btn = document.querySelector(".before_page_btn");
+const after_btn = document.querySelector(".after_page_btn");
+
+before_btn.addEventListener("click", () => {
+  if (currentpage > 1) {
+    currentpage -= 1;
+    createPage(currentpage);
+  }
+});
+
+after_btn.addEventListener("click", () => {
+  if (currentpage < totalPageCount) {
+    currentpage += 1;
+    createPage(currentpage);
+  }
+});
